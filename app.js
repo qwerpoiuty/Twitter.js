cae1f9eea71688d2b8695c86dad0582d485d223a
Stan Le <stan.le1107@gmail.com>
2015-04-29 13:14:07 -0700

    Updated twitter feed + socket io

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var logger = require("morgan");
 var path = require("path");
 var swig = require('swig');
 var routes = require('./routes/');
+var http = require('http').Server(app);
+var io  = require('socket.io')(http);
+
+io.on('connection', function(socket){
+    console.log("connected")
+});
 
 app.use('/', routes);
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,27 @@ var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
+var bodyParser = require('body-parser')
+
+router.use(bodyParser.urlencoded({extended:false}));
+router.use(bodyParser.json());
 
 router.get('/users/:name', function(req, res) {
     var name = req.params.name;
     var list = tweetBank.find( {name: name} );
-    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm:true, name:name } );
 });
 
 router.get('/', function (req, res) {
     var tweets = tweetBank.list();
-    res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets ,showForm: true} );
 });
 
+router.post('/submit', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('/');
+});
 
 module.exports = router;
