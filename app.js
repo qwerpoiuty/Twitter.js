b3b74dcd2fb456aabd25cc099d5921e22a6014d9
Stan Le <stan.le1107@gmail.com>
2015-04-29 11:56:09 -0700

    hi sang

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ var express = require("express");
 var app = express();
 var logger = require("morgan");
 var path = require("path");
+var swig = require('swig');
+var routes = require('./routes/');
 
+app.use('/', routes);
 app.use(logger('dev'));
 app.listen(3000);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views',path.join(__dirname,'/views'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
-   res.send("a;sldkjf");
-});
+
+
+swig.setDefaults({ cache: false });
+
+
+
+
