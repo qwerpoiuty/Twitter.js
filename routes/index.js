63a7acbca77ef6aba61588ef19f3b9c51d2c0885
Stan Le <stan.le1107@gmail.com>
2015-04-29 12:28:44 -0700

    Updated routing

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
+router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+});
+
 router.get('/', function (req, res) {
     var tweets = tweetBank.list();
     res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
+
 
 module.exports = router;
